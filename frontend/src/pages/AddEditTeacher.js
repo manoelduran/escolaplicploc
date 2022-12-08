9be557cfc9ec71eae6bbdd6ff449a23507a1a21a
DiegoSilvaMenezes <Menezes_59@hotmail.com>
2022-12-08 20:25:22 -0300

    fix(pages): fix formValue

--- frontend/src/pages/AddEditTeacher.js
+++ frontend/src/pages/AddEditTeacher.js
@@ -1,74 +1,111 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useTeachers } from '../context/TeachersContext';
-import { fetchAPI } from '../service/api';
-import '../styles/addEditTeacher.css';
+import { useTeachers } from "../context/TeachersContext";
+import { fetchAPI } from "../service/api";
+import "../styles/addEditTeacher.css";
 
 const initialState = {
-    name: "",
-    CPF: "",
-    academicTitle: "",
-    discipline: ""
+  name: "",
+  CPF: "",
+  academicTitle: "",
+  discipline: "",
 };
 
 function AddEditTeacher() {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [editMode, setEditMode] = useState(false);
-    const {updateTeacher, createTeacher} = useTeachers();
-    const [formValue, setFormValue] = useState(initialState);
-    const { name, CPF, academicTitle, discipline } = formValue;
-    const showTeacher = async (teacher_id) => {
-        console.log('teacher_id', teacher_id)
-        const selectedTeacher = await fetchAPI(`/teachers/${teacher_id}`, 'get')
-        const data = await selectedTeacher.json()
-        setFormValue(data)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [editMode, setEditMode] = useState(false);
+  const { updateTeacher, createTeacher } = useTeachers();
+  const [formValue, setFormValue] = useState(initialState);
+  const { name, CPF, academicTitle, discipline } = formValue;
+  const showTeacher = async (teacher_id) => {
+    console.log("teacher_id", teacher_id);
+    const selectedTeacher = await fetchAPI(`/teachers/${teacher_id}`, "get");
+    const data = await selectedTeacher.json();
+    setFormValue(data);
+  };
+  useEffect(() => {
+    if (id) {
+      setEditMode(true);
+      const selectedTeacher = showTeacher(id);
+      setFormValue({ ...selectedTeacher });
     }
-    useEffect(() => {
-        if (id) {
-            setEditMode(true);
-            const selectedTeacher = showTeacher(id)
-            setFormValue({ ...selectedTeacher });
-        }
-    }, [id]);
+  }, [id]);
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (name && CPF && academicTitle && discipline && editMode) {
+      await updateTeacher({ id, formValue });
+      setEditMode(false);
+      setTimeout(() => navigate("/"), 500);
+      return;
+    }
+    console.log("event", formValue);
+    await fetchAPI("/teachers", "post", formValue);
+    setTimeout(() => navigate("/"), 500);
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        if (name && CPF && academicTitle && discipline && editMode) {
-           await updateTeacher({ id, formValue })
-            setEditMode(false);
-            setTimeout(() => navigate("/"), 500);
-            return;
-        };
-        console.log('event', formValue)
-       await fetchAPI("/teachers", 'post', {formValue})
-        setTimeout(() => navigate("/"), 500);
-    };
-
-    const onInputChange = (event) => {
-        event.preventDefault();
-        let { name, value } = event.target;
-        setFormValue({ ...formValue, [name]: value });
-
-    };
-    return (
-        <div className="addTeacherContainer">
-            <div className="addTeacherInfo">
-                <h1 className="addTeacherTitle">Cadastre um professor:</h1>
-                <button className="addTeacherButton" onClick={() => navigate(-1)}>Voltar</button>
-            </div>
-            <div className="formContainer">
-                <form onSubmit={handleSubmit}>
-                    <input type='text' name='name' placeholder='Nome' required value={name || ""} onChange={onInputChange} />
-                    <input type='cpf' name='CPF' title='CPF' placeholder='CPF' required value={CPF || ""} onChange={onInputChange} />
-                    <input type='text' name='academicTitle' title='Título Acadêmico' placeholder='Título Acadêmico' required value={academicTitle || ""} onChange={onInputChange} />
-                    <input type='text' name='discipline' title='Disciplina' placeholder='Disciplina' required value={discipline || ""} onChange={onInputChange} />
-                    <button type='submit' className='addStudentButton' style={{ marginTop: 15 }}>{editMode ? "Editar" : "Criar"}</button>
-                </form>
-            </div>
-        </div>
-    );
+  const onInputChange = (event) => {
+    event.preventDefault();
+    let { name, value } = event.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
+  return (
+    <div className="addTeacherContainer">
+      <div className="addTeacherInfo">
+        <h1 className="addTeacherTitle">Cadastre um professor:</h1>
+        <button className="addTeacherButton" onClick={() => navigate(-1)}>
+          Voltar
+        </button>
+      </div>
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome"
+            required
+            value={name || ""}
+            onChange={onInputChange}
+          />
+          <input
+            type="cpf"
+            name="CPF"
+            title="CPF"
+            placeholder="CPF"
+            required
+            value={CPF || ""}
+            onChange={onInputChange}
+          />
+          <input
+            type="text"
+            name="academicTitle"
+            title="Título Acadêmico"
+            placeholder="Título Acadêmico"
+            required
+            value={academicTitle || ""}
+            onChange={onInputChange}
+          />
+          <input
+            type="text"
+            name="discipline"
+            title="Disciplina"
+            placeholder="Disciplina"
+            required
+            value={discipline || ""}
+            onChange={onInputChange}
+          />
+          <button
+            type="submit"
+            className="addStudentButton"
+            style={{ marginTop: 15 }}
+          >
+            {editMode ? "Editar" : "Criar"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export { AddEditTeacher };
--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,11 +1,17 @@
-
 async function fetchAPI(url, method, body) {
-
-    return await fetch(`http://localhost:3336${url}`, {
-        method: method,
-        body: body,
-    })
-
+  return await fetch(`http://localhost:3336${url}`, {
+    method: method,
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(body),
+  });
 }
 
 export { fetchAPI };
