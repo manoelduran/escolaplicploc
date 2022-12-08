59294e8d3288eabb283b784726530b70318cfd56
DiegoSilvaMenezes <Menezes_59@hotmail.com>
2022-12-08 18:00:23 -0300

    fix(students): fix repository

--- backend/src/modules/students/controllers/StudentsController.js
+++ backend/src/modules/students/controllers/StudentsController.js
@@ -1,51 +1,67 @@
+import { ClassRoomsRepository } from "../../classrooms/repositories/ClassRoomsRepository.js";
 import { Student } from "../models/Student.js";
 import { StudentsRepository } from "../repositories/StudentsRepository.js";
 
 export class StudentController {
-  constructor() {
-    this.studentRepository = new StudentsRepository();
-  }
-
   async create(req, res) {
-    const { data } = req.body;
+    const data = req.body;
 
-    if (!data.name || !data.CPF || !data.enrollment || !data.classroom) {
+    if (!data || !data.name || !data.CPF || !data.registrationNumber) {
       return res
         .status(400)
-        .send("Nome, CPF, matrícula e sala são obrigatórios");
+        .json({ message: "name, CPF e registrationNumber são obrigatórios" });
     }
 
-    const student = new Student(data);
+    const classRoomRepository = new ClassRoomsRepository();
+    const studentsRepository = new StudentsRepository();
+    const firstClassExists = await classRoomRepository.getById(1);
 
     try {
-      const studentCreated = await this.studentRepository.create(student);
+      if (!firstClassExists) {
+        return res.status(400).json({
+          message: "Não ha classes existentes para criar um estudante",
+        });
+      }
+
+      const student = new Student({ classroom: firstClassExists, ...data });
+
+      const studentCreated = await studentsRepository.create(student);
 
       return res.status(201).json(studentCreated);
     } catch (error) {
-      throw error;
+      console.log(error);
+      return res.status(500).send({ error: "Erro ao criar aluno" });
     }
   }
 
   async show(req, res) {
     const id = req.params.id;
     if (!id) {
-      throw new Error("ID é obrigatório");
+      return res.status(400).send({ error: "ID é obrigatório" });
     }
 
     try {
-      const student = await this.studentRepository.getById(id);
+      const studentsRepository = new StudentsRepository();
+      const student = await studentsRepository.getById(id);
+
+      if (!student) {
+        return res.status(404).send({ error: "student não encontrado" });
+      }
 
       return res.status(200).json(student);
     } catch (error) {
-      throw error;
+      console.log(error);
+      return res.status(500).send({ error: "Erro ao buscar aluno" });
     }
   }
 
   async listStudents(req, res) {
     try {
-      const students = await this.studentRepository.list();
-      return res.send(students);
+      const studentsRepository = new StudentsRepository();
+      const students = await studentsRepository.getAll();
+      return res.json(students);
     } catch (error) {
+      console.log(error);
       return res.status(500).send({ error: "Erro ao listar alunos" });
     }
   }
@@ -58,21 +74,58 @@ export class StudentController {
 
     try {
       const data = req.body;
-      const studentRepository = new StudentsRepository();
-      if (!data.name || !data.CPF || !data.enrollment || !data.classroom) {
-        return res
-          .status(400)
-          .send("Nome, CPF, matrícula e sala são obrigatórios");
-      }
+      const studentsRepository = new StudentsRepository();
+      const classRoomRepository = new ClassRoomsRepository();
 
-      const student = await studentRepository.getById(id);
-      
-      if(!student) {
+      const student = await studentsRepository.getById(id);
+
+      if (!student) {
         return res.status(400).send({
           message: "Estudante não encontrado",
         });
       }
-      const updatedStudent = await studentRepository.update({...data, id: student.id});
+
+      let classroom = student.classroom;
+
+      if (data.classroom_id) {
+        const classroomExists = await classRoomRepository.getById(
+          data.classroom_id
+        );
+
+        if (!classroomExists) {
+          return res.status(400).json({
+            message: "classroom não encontrada",
+          });
+        }
+
+        const reportCardExists = await reportCardsRepository.getById(id);
+
+        if (!reportCardExists) {
+          return res.status(400).send({
+            message: "Não é possivel atualizar a classroom pois não ha boletim",
+          });
+        }
+
+        if (!reportCardExists.approved) {
+          return res.status(400).send({
+            message:
+              "Não é possivel atualizar a classroom pois o aluno não foi aprovado",
+          });
+        }
+
+        classroom = classroomExists;
+      }
+
+      const updatedStudent = await studentsRepository.update(
+        new Student({
+          name: data.name || student.name,
+          CPF: data.CPF || student.CPF,
+          registrationNumber:
+            data.registrationNumber || student.registrationNumber,
+          classroom,
+        })
+      );
+
       return res.status(200).send(updatedStudent);
     } catch (error) {
       console.log(error);
@@ -88,7 +141,17 @@ export class StudentController {
     }
 
     try {
-      const student = await this.studentRepository.delete(id);
+      const studentsRepository = new StudentsRepository();
+
+      const student = await studentsRepository.getById(id);
+
+      if (!student) {
+        return res.status(400).send({
+          message: "Estudante não encontrado",
+        });
+      }
+
+      await studentsRepository.delete(id);
       return res.status(200).json(student);
     } catch (error) {
       return res.status(500).json({ error: "Erro ao deletar aluno" });
--- backend/src/modules/students/repositories/StudentsRepository.js
+++ backend/src/modules/students/repositories/StudentsRepository.js
@@ -1,35 +1,47 @@
 import pg from "pg";
 
 import { databaseConfig } from "../../../config.js";
+import { Student } from "../models/Student.js";
+import { ClassRoom } from "../../classrooms/models/ClassRoom.js";
 
 export class StudentsRepository {
+  selectFormatted =
+    'id, name, cpf AS "CPF", registrationnumber AS "registrationNumber", classroom_id';
   constructor() {
     this.pool = new pg.Pool(databaseConfig);
     this.pool.connect();
   }
 
-  async create(student) {
-    const { name, CPF, logo, address } = student;
-    const query =
-      "INSERT INTO students (name, cpf, logo, address) VALUES ($1, $2, $3, $4) RETURNING *";
+  mapToModel(student) {
+    const { classroom_id, ...result } = student;
+    return new Student({
+      ...result,
+      classroom: new ClassRoom({ id: classroom_id }),
+    });
+  }
 
-    const values = [name, CPF, logo, address];
+  async create(student) {
+    const { name, CPF, registrationNumber, classroom } = student;
+    const query = `INSERT INTO students (name, cpf, registrationnumber, classroom_id) VALUES ($1, $2, $3, $4) RETURNING ${this.selectFormatted}`;
+
+    const values = [name, CPF, registrationNumber, classroom.id];
 
     try {
       const result = await this.pool.query(query, values);
-      return result.rows[0];
+      return this.mapToModel(result.rows[0]);
     } catch (error) {
       throw error;
     }
   }
 
   async getById(id) {
-    const query = "SELECT * FROM students WHERE id = $1";
+    const query = `SELECT ${this.selectFormatted} FROM students WHERE id = $1`;
+
     const values = [id];
 
     try {
       const result = await this.pool.query(query, values);
-      return result.rows[0];
+      return result.rows.length == 0 ? null : this.mapToModel(result.rows[0]);
     } catch (error) {
       throw error;
     }
@@ -38,9 +50,10 @@ export class StudentsRepository {
   async getAll() {
     try {
       const result = await this.pool.query(
-        "SELECT * FROM students WHERE id = $1"
+        `SELECT ${this.selectFormatted} FROM students`
       );
-      return result.rows;
+
+      return result.rows.map(this.mapToModel);
     } catch (error) {
       throw error;
     }
@@ -48,25 +61,24 @@ export class StudentsRepository {
 
   async update(school) {
     const { id, name, CPF, logo, address } = school;
-    const query =
-      "UPDATE students SET name = $1, cpf = $2, logo = $3, address = $4 WHERE id = $5 RETURNING * ";
+    const query = `UPDATE students SET name = $1, cpf = $2, logo = $3, address = $4 WHERE id = $5 RETURNING ${this.selectFormatted} `;
     const values = [name, CPF, logo, address, id];
 
     try {
       const result = await this.pool.query(query, values);
-      return result.rows[0];
+      return this.mapToModel(result.rows[0]);
     } catch (error) {
       throw error;
     }
   }
 
   async delete(id) {
-    const query = "DELETE FROM students WHERE id = $1 RETURNING *";
+    const query = `DELETE FROM students WHERE id = $1 RETURNING ${this.selectFormatted}`;
     const values = [id];
 
     try {
       const result = await this.pool.query(query, values);
-      return result.rows[0];
+      return this.mapToModel(result.rows[0]);
     } catch (error) {
       throw error;
     }
