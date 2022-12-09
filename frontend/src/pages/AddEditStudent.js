4149dc15b111b609842ae60c745eea9c36f980ec
DiegoSilvaMenezes <Menezes_59@hotmail.com>
2022-12-09 19:48:44 -0300

    feat(StudentProfile): add student registry for classes

--- frontend/src/pages/AddEditStudent.js
+++ frontend/src/pages/AddEditStudent.js
@@ -1,70 +1,102 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { useStudents } from '../context/StudentsContext';
-import { fetchAPI } from '../service/api';
-import '../styles/addEditStudent.css';
-
+import { useStudents } from "../context/StudentsContext";
+import { fetchAPI } from "../service/api";
+import "../styles/addEditStudent.css";
 
 function AddEditStudent() {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [editMode, setEditMode] = useState(false);
-    const { updateStudent } = useStudents();
-    const [formValue, setFormValue] = useState({
-        name: "",
-        CPF: "",
-        registrationNumber: "",
-    });
-    const { name, CPF, registrationNumber } = formValue;
-    console.log(formValue);
-    const showStudent = async (student_id) => {
-        const selectedStudent = await fetchAPI(`/students/${student_id}`, "GET");
-        const data = await selectedStudent.json();
-        setFormValue(data);
-    };
-    useEffect(() => {
-        if (id) {
-            setEditMode(true);
-            const selectedStudent = showStudent(id);
-            setFormValue({ ...selectedStudent });
-        }
-    }, [id]);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [editMode, setEditMode] = useState(false);
+  const { updateStudent } = useStudents();
+  const [formValue, setFormValue] = useState({
+    name: "",
+    CPF: "",
+    registrationNumber: "",
+  });
+  const { name, CPF, registrationNumber } = formValue;
+  const showStudent = async (student_id) => {
+    const selectedStudent = await fetchAPI(`/students/${student_id}`, "GET");
+    const data = await selectedStudent.json();
+    setFormValue(data);
+  };
+  useEffect(() => {
+    if (id) {
+      setEditMode(true);
+      const selectedStudent = showStudent(id);
+      setFormValue({ ...selectedStudent });
+    }
+  }, [id]);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        if (name && CPF && registrationNumber && editMode) {
-            await updateStudent({ id, formValue });
-            setEditMode(false);
-            setTimeout(() => navigate("/"), 500);
-            return;
-        }
-        console.log("event", formValue);
-        await fetchAPI("/students", "POST", formValue);
-        setTimeout(() => navigate("/"), 500);
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (name && CPF && registrationNumber && editMode) {
+      await updateStudent({ id, formValue });
+      setEditMode(false);
+      setTimeout(() => navigate("/"), 500);
+      return;
+    }
+    console.log("event", formValue);
+    await fetchAPI("/students", "POST", formValue);
+    setTimeout(() => navigate("/"), 500);
+  };
 
-    const onInputChange = (event) => {
-        event.preventDefault();
-        let { name, value } = event.target;
-        setFormValue({ ...formValue, [name]: value });
-    };
+  const onInputChange = (event) => {
+    event.preventDefault();
+    let { name, value } = event.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
 
-    return (
-        <div className="addStudentContainer">
-            <div className="addStudentInfo">
-                <h1 className="addStudentTitle"> {editMode ? `Edite o estudante:` : "Cadastre um estudante:"}</h1>
-                <button className="addStudentButton" onClick={() => navigate(-1)}>Voltar</button>
-            </div>
-            <div className="formContainer">
-                <form onSubmit={handleSubmit}>
-                    <input type='text' name='name' placeholder='Nome' required value={name || ""} onChange={onInputChange} />
-                    <input type='cpf' name='CPF' title='CPF' placeholder='CPF' required value={CPF || ""} onChange={onInputChange} />
-                    <input type='text' name='registrationNumber' title='Matrícula' placeholder='Número de Matrícula' required value={registrationNumber || ""} onChange={onInputChange} />
-                    <button type='submit' className='addStudentButton' style={{ marginTop: 15 }}>{editMode ? "Editar" : "Criar"}</button>
-                </form>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="addStudentContainer">
+      <div className="addStudentInfo">
+        <h1 className="addStudentTitle">
+          {" "}
+          {editMode ? `Edite o estudante:` : "Cadastre um estudante:"}
+        </h1>
+        <button className="addStudentButton" onClick={() => navigate(-1)}>
+          Voltar
+        </button>
+      </div>
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome"
+            required
+            value={name || ""}
+            onChange={onInputChange}
+          />
+          <input
+            type="cpf"
+            name="CPF"
+            title="CPF"
+            placeholder="CPF"
+            required
+            value={CPF || ""}
+            onChange={onInputChange}
+          />
+          <input
+            type="text"
+            name="registrationNumber"
+            title="Matrícula"
+            placeholder="Número de Matrícula"
+            required
+            value={registrationNumber || ""}
+            onChange={onInputChange}
+          />
+          <button
+            type="submit"
+            className="addStudentButton"
+            style={{ marginTop: 15 }}
+          >
+            {editMode ? "Editar" : "Criar"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export { AddEditStudent };
--- frontend/src/pages/StudentProfile.js
+++ frontend/src/pages/StudentProfile.js
@@ -7,29 +7,50 @@ import "../styles/teacherProfile.css";
 function StudentProfile() {
   const { id } = useParams();
   const navigate = useNavigate();
+
   const [student, setStudent] = useState({});
-  const [classRooms, setClassRooms] = useState([])
+  const [classRooms, setClassRooms] = useState([]);
+
+  const [selectedClassroomId, setSelectedClassroomId] = useState(0);
+
   const fetchStudent = async () => {
     const response = await fetchAPI(`/students/${Number(id)}`, "GET");
     const studentData = await response.json();
     setStudent(studentData);
   };
+
   const fetchClassRooms = async () => {
     const response = await fetchAPI(`/classrooms`, "GET");
     const classRoomsData = await response.json();
-    setClassRooms(classRoomsData);
+    setClassRooms(
+      classRoomsData.filter((classroom) => {
+        return classroom.id !== id;
+      })
+    );
   };
+
   const handleChange = (event) => {
     event.preventDefault();
-    setClassRooms(event.target.value)
-  }
-  console.log(student);
+    setSelectedClassroomId(event.target.value.id);
+  };
+
+  const handleRegistry = async () => {
+    try {
+      const response = await fetchAPI(`/students/${student.id}`, "PUT", {
+        classroom_id: selectedClassroomId,
+      });
+
+      await response.json();
+
+      navigate(`/classrooms/${selectedClassroomId}`);
+    } catch (error) {}
+  };
 
   useEffect(() => {
     fetchStudent();
-    fetchClassRooms()
+    fetchClassRooms();
   }, [id]);
-console.log('classRooms', classRooms)
+
   return (
     <div className="studentProfileContainer">
       <div className="studentProfileInfoContainer">
@@ -41,11 +62,28 @@ console.log('classRooms', classRooms)
         </h4>
       </div>
       <div className="buttonsDiv">
-        <select onChange={handleChange} style={{paddingRight: 20, alignSelf: 'center', justifySelf: 'center', marginRight: 50, width: 200, height: 50}}>
+        <select
+          onChange={handleChange}
+          style={{
+            paddingRight: 20,
+            alignSelf: "center",
+            justifySelf: "center",
+            marginRight: 50,
+            width: 200,
+            height: 50,
+          }}
+        >
           {classRooms?.map((classRoom, index) => (
-            <option key={index} value={classRoom.id} > Turma:{classRoom.subject}</option>
+            <option key={index} value={classRoom.id}>
+              {" "}
+              Turma:{classRoom.subject}
+            </option>
           ))}
         </select>
+        <button className="addStudentButton" onClick={handleRegistry}>
+          Matricular estudante
+        </button>
+
         <button
           className="addStudentButton"
           onClick={() => navigate(`/AddEditstudent/${student.id}`)}
