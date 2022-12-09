3b15b877ce6723ccc1040c3ce5c6e8bbe9e4882e
DiegoSilvaMenezes <Menezes_59@hotmail.com>
2022-12-09 00:08:44 -0300

    fix(Student): update profile

---
+++ frontend/src/components/ReportCard/ReportCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./ReportCard.css";
+
+const ReportCard = ({ reportcard, classroom, teacher }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nota</th>
+          <th>Turma</th>
+          <th>Professor</th>
+          <th>Aprovação</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{reportcard.finalGrade}</td>
+          <td>{classroom?.subject}</td>
+          <td>{teacher?.name}</td>
+          <td>{reportcard?.approved ? "Sim" : "Não"}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export { ReportCard };
--- frontend/src/pages/StudentProfile.js
+++ frontend/src/pages/StudentProfile.js
@@ -1,37 +1,62 @@
-import React, {useState, useEffect} from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import {fetchAPI} from '../service/api';
-import '../styles/teacherProfile.css';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ReportCard } from "../components/ReportCard/ReportCard";
+import { fetchAPI } from "../service/api";
+import "../styles/teacherProfile.css";
 
 function StudentProfile() {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    console.log('id', id)
-    const [student, setStudent] = useState({});
-    const fetchStudent = async (id) => {
-        const response = await fetchAPI(`/teachers/${Number(id)}`, 'GET')
-        const studentData = await response.json()
-        console.log('selectedTeacher', studentData)
-        setStudent(studentData)
-    }
-    useEffect(() => {
-        fetchStudent(id)
-    }, [id])
-    return (
-        <div className='studentProfileContainer'>
-            <div className="studentProfileInfoContainer">
-                <h1 className='studentTitle'>Informações do Professor:</h1>
-                <h4 className='studentName'>Nome: {student.name}</h4>
-                <h4 className='studentCPF'>CPF: {student.cpf}</h4>
-                <h4 className='studentAcademicTitle'>Título Acadêmico: {student.academicTitle}</h4>
-                <h4 className='studentDiscipline'>Disciplina: {student.discipline}</h4>
-            </div>
-            <div className='buttonsDiv'>
-                    <button className="addStudentButton" onClick={() => navigate(`/AddEditstudent/${student.id}`)}>Editar Estudante</button>
-                    <button className="classRoomButton" onClick={() => navigate(-1)}>Voltar</button>
-                </div>
-        </div>
-    );
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [student, setStudent] = useState({});
+
+  const fetchStudent = async () => {
+    const response = await fetchAPI(`/students/${Number(id)}`, "GET");
+    const studentData = await response.json();
+    setStudent(studentData);
+  };
+
+  console.log(student);
+
+  useEffect(() => {
+    fetchStudent();
+  }, [id]);
+
+  return (
+    <div className="studentProfileContainer">
+      <div className="studentProfileInfoContainer">
+        <h1 className="studentTitle">Informações do estudante:</h1>
+        <h4 className="studentName">Nome: {student.name}</h4>
+        <h4 className="studentCPF">CPF: {student.CPF}</h4>
+        <h4 className="studentAcademicTitle">
+          Número da matricula: {student.registrationNumber}
+        </h4>
+      </div>
+      <div className="buttonsDiv">
+        <button
+          className="addStudentButton"
+          onClick={() => navigate(`/AddEditstudent/${student.id}`)}
+        >
+          Editar Estudante
+        </button>
+        <button className="classRoomButton" onClick={() => navigate(-1)}>
+          Voltar
+        </button>
+      </div>
+
+      <div className="reportCardContainer">
+        <h1>Boletim</h1>
+        {student?.reportcards?.map((reportCard, index) => (
+          <ReportCard
+            key={index}
+            reportcard={reportCard}
+            teacher={student.teacher}
+            classroom={student.classroom}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export {StudentProfile};
+export { StudentProfile };
